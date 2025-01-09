5bb7dd24c47ccf068d116d617f39b6fc29176861
Chinmay Hegde <cod1.1backup@gmail.com>
2025-01-09 14:16:14 +0530

    Added live demo link to project card.

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import SubSkills from "./SubSkills";
+import axios from "axios";
 
 const ProjectCard = (project) => {
+
+  const [liveWebsite, setLiveWebsite] = useState(false);
+  const fetchLiveWebsite = async (author,name) => {
+
+    const response = await axios.get(`https://api.github.com/repos/${author}/${name}`);
+    const data = response.data;
+    if(!data.homepage){
+      setLiveWebsite(false);
+      return false;
+    }
+    else{
+      setLiveWebsite(data.homepage);
+      return true;
+    }
+  }
+
+
+
   let skills = [];
   if (project.project.name === "SecureShop") {
     skills = [
@@ -19,7 +38,7 @@ const ProjectCard = (project) => {
     skills = ["HTML", "CSS", "Javascript", "PHP", "XAMPP", "MySQL"];
   }
 
-  else if (project.project.name === "stackoverflow-api" || "School-Management-System") {
+  else if (project.project.name === "stackoverflow-api" || project.project.name === "School-Management-System") {
     skills = [
       "NodeJS",
       "MongoDB",
@@ -27,7 +46,7 @@ const ProjectCard = (project) => {
     ];
   }
 
-  else if(project.project.name === "Restaurant-Table-Booking-System" || "Travel-Agency-Booking-System"){
+  else if(project.project.name === "Restaurant-Table-Booking-System" || project.project.name ===  "Travel-Agency-Booking-System"){
     skills = [
       "ReactJS",
       "NodeJS",
@@ -44,21 +63,45 @@ const ProjectCard = (project) => {
         </span>
         <span className="mr-2 mt-2">
           {project.project.github ? (
+            <div>
+
             <a
               href={`https://github.com/${project.project.github} `}
               target="_blank"
               rel="noopener noreferrer"
-            >
+              >
               <GitHubIcon style={{ fontSize: "20px", color: "black" }} />
             </a>
+            </div>
+            
           ) : (
+            <div>
+              
             <a
               href={`https://github.com/${project.project.author}/${project.project.name} `}
               target="_blank"
               rel="noopener noreferrer"
-            >
+              >
               <GitHubIcon style={{ fontSize: "20px", color: "black" }} />
             </a>
+            {fetchLiveWebsite(project.project.author, project.project.name) ? (
+              <a
+              href={liveWebsite}
+              target="_blank"
+              rel="noopener noreferrer"
+              >
+              <span className="font-bold text-[15px] text-left leading-5 p-2 hover:text-blue-500">
+                Live
+              </span>
+            </a>
+            ) : (
+              <> NOT LIVE</>
+            )
+            }
+            
+              </div>
+
+
           )}
         </span>
       </div>
